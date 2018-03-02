963f6e7723d2e8b7b48b1db6ce8772077b8e3436
wangcheng <veoxft@hotmail.com>
2018-03-02 10:47:14 +0800

    Builder pattern for class hierarchies

--- src/main/java/Item2ConsiderBuilder/Calzone.java
+++ src/main/java/Item2ConsiderBuilder/Calzone.java
@@ -3,5 +3,26 @@ package Item2ConsiderBuilder;
 /**
  * Created by wangcheng  on 2018/3/1.
  */
-public class Calzone {
+public class Calzone extends Pizza{
+    private final boolean sauceInside;
+    public static class Builder extends Pizza.Builder<Builder>{
+        private boolean sauceInside = false;//Default
+        public Builder sauceInside(){
+            sauceInside = true;
+            return this;
+        }
+        @Override
+        public Calzone build(){
+            return new Calzone(this);
+        }
+        @Override
+        protected Builder self(){return this;}
+    }
+    private Calzone(Builder builder){
+        super(builder);
+        sauceInside = builder.sauceInside;
+    }
+    public static void main(String[] args){
+        Calzone calzone = new Calzone.Builder().addTopping(Topping.HAM).sauceInside().build();
+    }
 }
