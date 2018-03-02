eea45559f4163c877f0b3978689ff2413b4d0baf
wangcheng <veoxft@hotmail.com>
2018-03-02 18:35:05 +0800

    Noninstantiable utility class

--- src/main/java/Item4usePrivateConstructor/UtilityClass.java
+++ src/main/java/Item4usePrivateConstructor/UtilityClass.java
@@ -1,7 +1,13 @@
 package Item4usePrivateConstructor;
-
+//if a class that is just a grouping of static methods and
+//static fields.you can use it.
 /**
  * Created by wangcheng  on 2018/3/2.
  */
+//Noninstantiable utility class
 public class UtilityClass {
+    private UtilityClass(){
+        throw new RuntimeException();
+    }
+    //...
 }
