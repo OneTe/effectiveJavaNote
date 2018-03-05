1ce8a4204d85cb7e1435e3c707fee50f75481db7
wangcheng <veoxft@hotmail.com>
2018-03-05 16:01:33 +0800

    avoid creating unnecessary objects

--- src/main/java/Item6avoidCreatingUnnecessaryObjects/RomanNumerals.java
+++ src/main/java/Item6avoidCreatingUnnecessaryObjects/RomanNumerals.java
@@ -1,7 +1,15 @@
 package Item6avoidCreatingUnnecessaryObjects;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by wangcheng  on 2018/3/5.
  */
 public class RomanNumerals {
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+            + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    static boolean isRomanNumeral(String s){
+        return ROMAN.matcher(s).matches();
+    }
 }
