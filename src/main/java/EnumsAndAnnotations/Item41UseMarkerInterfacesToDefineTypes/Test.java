2751a4a218d661b153bd4e08ee4ba95133ebb0e1
wangcheng <veoxft@hotmail.com>
2018-04-09 14:37:25 +0800

    use marker interfaces to define types

---
+++ src/main/java/EnumsAndAnnotations/Item41UseMarkerInterfacesToDefineTypes/Test.java
@@ -0,0 +1,9 @@
+package EnumsAndAnnotations.Item41UseMarkerInterfacesToDefineTypes;
+
+import java.io.Serializable;
+
+/**
+ * Created by wangcheng  on 2018/4/9.
+ */
+public class Test implements Serializable{
+}
