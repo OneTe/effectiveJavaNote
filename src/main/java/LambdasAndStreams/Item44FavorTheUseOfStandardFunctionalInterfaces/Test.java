44a13b56716ed0a5bf0b4e9234aa9d4f35c375a8
wangcheng <veoxft@hotmail.com>
2018-04-09 17:33:03 +0800

    functional interfaces

---
+++ src/main/java/LambdasAndStreams/Item44FavorTheUseOfStandardFunctionalInterfaces/Test.java
@@ -0,0 +1,9 @@
+package LambdasAndStreams.Item44FavorTheUseOfStandardFunctionalInterfaces;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Created by wangcheng  on 2018/4/9.
+ */
+public class Test {
+}
