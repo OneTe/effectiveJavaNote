6d9014a99855fac4d6afb01923f578ac96d569d5
wangcheng <veoxft@hotmail.com>
2018-04-17 16:59:28 +0800

    prefer interfaces to reflection

---
+++ src/main/java/GeneralProgramming/Item65PreferInterfacesToReflection/Test.java
@@ -0,0 +1,55 @@
+package GeneralProgramming.Item65PreferInterfacesToReflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+//First, the example can generate six different exceptions at runtime
+//The second disadvantage is that it takes twenty-five lines of tedious code to generate an instance of the
+//class from its name, whereas a constructor invocation would fit neatly on a single line
+
+
+/**
+ * Created by wangcheng  on 2018/4/17.
+ */
+public class Test {
+    //Reflective instantiation with interface access
+    public static void main(String[] args){
+        //Translate the class name into a Class object
+        Class<? extends Set<String>> cl = null;
+        try{
+            cl = (Class<? extends Set<String >>) //Unchecked cast!
+                    Class.forName(args[0]);
+        }catch (ClassNotFoundException e){
+            fatalError("Class not found.");
+        }
+        //Get the constructor
+        Constructor<? extends Set<String>> cons = null;
+        try{
+            cons = cl.getDeclaredConstructor();
+        }catch (NoSuchMethodException e){
+            fatalError("No parameterless constructor");
+        }
+        //Instantiate the set
+        Set<String> s = null;
+        try{
+            s = cons.newInstance();
+        }catch (IllegalAccessException e){
+            fatalError("Constructor not accessible");
+        }catch (InstantiationException e){
+            fatalError("Class not instantiable");
+        }catch (InvocationTargetException e){
+            fatalError("Constructor threw " + e.getCause());
+        }catch (ClassCastException e){
+            fatalError("Class doesn't implement Set");
+        }
+        //Exercise the set
+        s.addAll(Arrays.asList(args).subList(1,args.length));
+        System.out.println(s);
+
+    }
+    private static void fatalError(String msg){
+        System.err.println(msg);
+        System.exit(1);
+    }
+}
